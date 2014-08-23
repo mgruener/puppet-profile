6095e02f023ff54d1b5c2433a78e1412848a8c7f
Michael Gruener <michael.gruener@chaosmoon.net>
2014-08-23 21:20:00 +0200

    Add ensure parameter to multifactor_auth profile. Allows to toggle the authentication on/off for a client.

--- manifests/multifactor_auth.pp
+++ manifests/multifactor_auth.pp
@@ -1,20 +1,23 @@
 class profile::multifactor_auth (
   $radius_secret,
   $services = {},
+  $ensure   = present,
 ) {
 
   include pam_radius
 
   $defaults = {
+    ensure   => $ensure,
     type     => 'auth',
     control  => 'required',
     module   => 'pam_radius_auth.so',
-    position => 'before first entry',
+    position => 'before first',
   }
 
   create_resources('pam',$services,$defaults)
 
   @@freeradius::client { $::fqdn:
+    ensure => $ensure,
     secret => $radius_secret,
     tag    => 'radius_client',
   }
