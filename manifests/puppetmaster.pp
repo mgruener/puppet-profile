abb952aa447e4df53fb6e6d7723281082d5ac97b
Michael Gruener <michael.gruener@chaosmoon.net>
2014-08-23 23:07:10 +0200

    Let puppet master handle the access permissions to its hiera data directory itself

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,10 +1,19 @@
 class profile::puppetmaster (
   $use_puppetdb = true,
+  $datadir      = '/etc/puppet/data',
 ) {
   include puppet::master
 
   if $use_puppetdb == true {
     include puppetdb
     include puppetdb::master::config
+  }
+
+  file { $datadir:
+    ensure  => directory,
+    owner   => 'puppet',
+    group   => 'puppet',
+    recurse => true,
+    mode    => 'go-rwx'
   }
 }
