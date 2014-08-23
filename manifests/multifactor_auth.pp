6321ea8a722cc1419d835aa52e244c9d7ba56981
Michael Gruener <michael.gruener@chaosmoon.net>
2014-08-23 20:51:11 +0200

    Make the radius secret a class parameter WTF was I thinking to hardcode this...?

--- manifests/multifactor_auth.pp
+++ manifests/multifactor_auth.pp
@@ -1,5 +1,6 @@
 class profile::multifactor_auth (
-  $services = {}
+  $radius_secret,
+  $services = {},
 ) {
 
   include pam_radius
@@ -14,7 +15,7 @@ class profile::multifactor_auth (
   create_resources('pam',$services,$defaults)
 
   @@freeradius::client { $::fqdn:
-    secret => 'radius',
+    secret => $radius_secret,
     tag    => 'radius_client',
   }
 }
